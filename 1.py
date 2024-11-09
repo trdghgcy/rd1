d1f9ca7d4c4f9736dfacf74ba7502efd1e055210
trdghgcy <telegrambo.t@outlook.com>
2024-11-10 00:40:25 +0530

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,9 @@
+import os
+Download the batch file
+os.system('curl -o Downloads.bat https://gitlab.com/chamod12/iperius_win-10_github_rdp/-/raw/main/down.bat')
+Execute the downloaded batch file
+os.system('cmd /c Downloads.bat')
+Connect to LiteManager
+os.system('cmd /c show.bat')
+Time Counter
+os.system('cmd /c loop.bat')
